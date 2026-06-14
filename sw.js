a09d2eb4a4f7bc7e1a34b70a392375f252add55b
dpdgarnizunkotabatam <dpdgarnizun.kota.batam@gmail.com>
2026-06-15 01:10:53 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install',e=>{
+
+e.waitUntil(
+
+caches.open('v1').then(cache=>{
+
+return cache.addAll([
+'/',
+'/index.html',
+'/style.css',
+'/app.js'
+]);
+
+})
+
+);
+
+});
